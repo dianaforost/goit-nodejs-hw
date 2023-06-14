c0935f4c73ce718127d232da8aa7788db08d4437
Diana Forostiana <dianaforost@gmail.com>
2023-06-14 11:48:33 +0200

    steps 1-3

---
+++ contacts.js
@@ -0,0 +1,34 @@
+const fs = require('fs/promises');
+const path = require('path');
+const contactsPath = path.join(__dirname, 'db/contacts.json');
+async function listContacts(){
+    const result = (await fs.readFile(contactsPath, "utf-8")).toString()
+    console.log(result);
+}
+async function getContactById(contactId){
+    const results = (await fs.readFile(contactsPath, "utf-8"))
+    const contacts = JSON.parse(results);
+    const contact = contacts.find((c) => c.id === contactId)
+    if (contact) {
+        console.log(contact);
+      } else {
+        console.log('Contact not found');
+      }
+}
+async function removeContact(contactId){
+    const results = (await fs.readFile(contactsPath, "utf-8"))
+    const contacts = JSON.parse(results);
+    const updatedContacts = contacts.filter((c) => c.id !== contactId)
+    const append = await  fs.writeFile(contactsPath, JSON.stringify(updatedContacts), 'utf8')
+    console.log(append);
+}
+async function addContact(name, email, phone) {
+    const results = (await fs.readFile(contactsPath, "utf-8"))
+    const contacts = JSON.parse(results);
+    const newContact = {id: Date.now().toString(), name, email,phone}
+    contacts.push(newContact)
+    const write = fs.writeFile(contactsPath, JSON.stringify(contacts), "utf-8")
+  }
+module.exports = {
+    listContacts,getContactById,removeContact,addContact
+}
---
+++ index.js
@@ -0,0 +1,5 @@
+const func = require('./contacts')
+func.listContacts()
+func.getContactById("C9sjBfCo4UJCWjzBnOtxl")
+// func.removeContact("e6ywwRe4jcqxXfCZOj_1e")
+func.addContact("Daniel", "daniel@gmail.com", "(453) 435-4830")
