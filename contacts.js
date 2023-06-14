d75c1319fdf9eb38e58f645978993f515c61f290
Diana Forostiana <dianaforost@gmail.com>
2023-06-14 13:36:30 +0200

    steps 4-5

--- contacts.js
+++ contacts.js
@@ -20,14 +20,15 @@ async function removeContact(contactId){
     const contacts = JSON.parse(results);
     const updatedContacts = contacts.filter((c) => c.id !== contactId)
     const append = await  fs.writeFile(contactsPath, JSON.stringify(updatedContacts), 'utf8')
-    console.log(append);
+    console.log(updatedContacts);
 }
 async function addContact(name, email, phone) {
     const results = (await fs.readFile(contactsPath, "utf-8"))
     const contacts = JSON.parse(results);
     const newContact = {id: Date.now().toString(), name, email,phone}
     contacts.push(newContact)
-    const write = fs.writeFile(contactsPath, JSON.stringify(contacts), "utf-8")
+    const write = fs.writeFile(contactsPath, JSON.stringify(contacts), "utf-8");
+    console.log(JSON.stringify(contacts));
   }
 module.exports = {
     listContacts,getContactById,removeContact,addContact
--- index.js
+++ index.js
@@ -1,5 +1,38 @@
+const { Command } = require("commander");
 const func = require('./contacts')
-func.listContacts()
-func.getContactById("C9sjBfCo4UJCWjzBnOtxl")
-// func.removeContact("e6ywwRe4jcqxXfCZOj_1e")
-func.addContact("Daniel", "daniel@gmail.com", "(453) 435-4830")
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+function invokeAction({ action, id, name, email, phone }) {
+    switch (action) {
+      case "list":
+        func.listContacts()
+        break;
+  
+      case "get":
+        func.getContactById(id)
+        break;
+  
+      case "add":
+        func.addContact(name, email, phone)
+        break;
+  
+      case "remove":
+        func.removeContact(id)
+        break;
+  
+      default:
+        console.warn("\x1B[31m Unknown action type!");
+    }
+  }
+  
+  invokeAction(argv);
