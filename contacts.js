98980ea9821b148b752578ae3f2e7d49c6595b71
Diana Forostiana <dianaforost@gmail.com>
2023-06-19 18:07:59 +0200

    fix: added try, catch

--- contacts.js
+++ contacts.js
@@ -6,7 +6,8 @@ async function listContacts(){
     console.log(result);
 }
 async function getContactById(contactId){
-    const results = (await fs.readFile(contactsPath, "utf-8"))
+    try{
+      const results = (await fs.readFile(contactsPath, "utf-8"))
     const contacts = JSON.parse(results);
     const contact = contacts.find((c) => c.id === contactId)
     if (contact) {
@@ -14,21 +15,35 @@ async function getContactById(contactId){
       } else {
         console.log('Contact not found');
       }
+    }
+    catch(e) {
+      console.log(e);
+    }
 }
 async function removeContact(contactId){
-    const results = (await fs.readFile(contactsPath, "utf-8"))
+    try{
+      const results = (await fs.readFile(contactsPath, "utf-8"))
     const contacts = JSON.parse(results);
     const updatedContacts = contacts.filter((c) => c.id !== contactId)
     const append = await  fs.writeFile(contactsPath, JSON.stringify(updatedContacts), 'utf8')
     console.log(updatedContacts);
+    }
+    catch(e) {
+      console.log(e);
+    }
 }
 async function addContact(name, email, phone) {
-    const results = (await fs.readFile(contactsPath, "utf-8"))
+    try{
+      const results = (await fs.readFile(contactsPath, "utf-8"))
     const contacts = JSON.parse(results);
     const newContact = {id: Date.now().toString(), name, email,phone}
     contacts.push(newContact)
     const write = fs.writeFile(contactsPath, JSON.stringify(contacts), "utf-8");
     console.log(JSON.stringify(contacts));
+    }
+    catch (e){
+      console.log(e);
+    }
   }
 module.exports = {
     listContacts,getContactById,removeContact,addContact
